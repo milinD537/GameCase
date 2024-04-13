fa3e278c80db35cc794ca2d7f2536a1fd103c84b
milinD537 <milindpithadia22@gmail.com>
2024-04-13 23:11:23 +0530

    first draft

--- src/App.jsx
+++ src/App.jsx
@@ -31,35 +31,37 @@ function App() {
 
   return (
     <div>
-      <button className='absolute top-2 left-1/2 bg-black p-4' onClick={() => {(gameJSON.previous) && setPage((prev) => prev - 1 )}}>Prev</button>
-      <button className='absolute top-2 left-[60%] bg-black p-4' onClick={() => ((gameJSON.next)) && setPage((prev) => prev + 1)}>Next</button>
-      <section className='header'></section>
+    
+      <button className='absolute z-50 top-2 left-1/2 bg-black p-4' onClick={() => {(gameJSON.previous) && setPage((prev) => prev - 1 )}}>Prev</button>
+      <button className='absolute z-50 top-2 left-[60%] bg-black p-4' onClick={() => ((gameJSON.next)) && setPage((prev) => prev + 1)}>Next</button>
+    
+      <section className='header | w-[min(1680px,100%_-_3rem)] mx-auto'>TITLE</section>
 
-      <section className='heroSection'>
+      <section className='heroSection | w-[min(1680px,100%_-_3rem)] mx-auto | mb-12'>
         <div className='trending'></div>
-        <div className='heroGrid'>
+        <div className='heroGrid | grid grid-rows-[auto_auto] min-[1200px]:grid-cols-[0.8fr_0.2fr] gap-8'>
           <div>
             {(gameJSON.results.length === 0) ? (<></>) :
               (
-                <div className="potentialHeroComponent | grid">
-                  <img src={gameJSON.results[heroCardIndex].background_image} className='col-start-1 col-span-full row-start-1 row-span-full' />
-                  <div className='col-start-1 col-span-full row-start-1 row-span-full text-black self-end w-fit'>
-                    <h3>{gameJSON.results[heroCardIndex].name}</h3>
+                <div className="potentialHeroComponent | grid grid-cols-[2rem_1fr] min-[980px]:grid-cols-[3rem_1fr] grid-rows-[1fr_2rem] min-[980px]:grid-rows-[1fr_3rem] rounded-3xl relative overflow-hidden | before:content-[''] before:absolute before:z-0 before:inset-0 before:bg-gradient-to-t before:from-black before:from-0%% before:to-transparent">
+                  <img src={gameJSON.results[heroCardIndex].background_image} className='heroImage | col-start-1 col-span-full row-start-1 row-span-full w-full aspect-[16/9] object-cover' />
+                  <div className='col-start-2 row-start-1 self-end relative'>
+                    <h3 className='text-xl min-[980px]:text-[3.5rem] font-bold'>{gameJSON.results[heroCardIndex].name}</h3>
                     <p></p>
-                    <button>LEARN MORE</button>
+                    <button className='cursor-pointer mt-4 min-[980px]:mt-10 bg-white text-black text-xs min-[980px]:text-xl py-[0.5em] px-[1em] rounded'>LEARN MORE</button>
                   </div>
                 </div>
               )}
           </div>
-          <div className='outline outline-red-300'>
+          <div className='grid gap-4 max-[1199px]:grid-cols-[repeat(3,1fr)]'>
             {gameJSON.results.length > 0 && gameJSON.results.slice(0,6).map((gameData, index) => <HeroGameCard key={gameData.id} item={gameData} onClickFn={() => setHeroCardIndex(index)}/>)}
           </div>
         </div>
       </section>
 
-      <section className=''>
+      <section className='cardsWrapper | w-[min(1680px,100%_-_3rem)] mx-auto'>
         <div className='sort |'></div>
-        <div className='cards'>
+        <div className='cards | grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-6 justify-center'>
           {gameJSON.results.length > 0 && gameJSON.results.slice(6).map((gameData) => <GameCard key={gameData.id} item={gameData}/>)}
         </div>
       </section>
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -3,17 +3,21 @@ import React from 'react'
 export default function GameCard({item}) {
   return (
     <>
-      <div className='card |'>
-        <div className='cardImageWrapper'>
-          <img src={item.background_image}/>
+      <div className='card | cursor-pointer'>
+        <div className='cardImageWrapper group | hover:contrast-75 hover:brightness-105 transition-all relative'>
+        <div className='absolute top-2 right-2 w-7 h-7 rounded-[50%] border-2 border-white bg-black grid place-items-center opacity-0 group-hover:opacity-100 transition-all'></div>
+          <img src={item.background_image} className='w-full aspect-[1/1.4] object-cover rounded-md'/>
         </div>
         <div className='cardDetails'>
-          <h3 className='gameTitle'>{item.name}</h3>
-          <div className='gameRatingWrapper'>
-            <img src='/ratingStar.png'/>
-            <span className='inline-block'>{item.rating}</span>
+          <h3 className='gameTitle | text-xl mb-2'>{item.name}</h3>
+          <div className='gameRatingWrapper | flex items-center gap-1 mb-1'>
+            <img src='/ratingStar.png' className='w-5 h-5'/>
+            <span className='px-2 py-1 rounded-md bg-[hsl(0,0%,16%)]'>{item.rating}</span>
           </div>
-          <p className='gamePlaytime'><span>{item.playtime}</span>hrs</p>
+          <div className='gamePlaytime | flex items-center gap-1'>
+            <img src='/playTime.png' className='w-5 h-5' />
+            <span className='px-2 py-1 rounded-md bg-[hsl(0,0%,16%)]'>{item.playtime} hrs</span>
+          </div>
         </div>
       </div>
     </>
